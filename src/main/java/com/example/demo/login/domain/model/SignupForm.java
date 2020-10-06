cf3ee7d72e1397d45ee8d0fafebba55fe5529ae1
katsubehiroshi <katsubehiroshi@sso.example.com>
2020-10-07 00:32:59 +0900

    syuuseitochu

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -1,4 +1,8 @@
 package com.example.demo.login.domain.model;
+
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -6,8 +10,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
-
 public class SignupForm {
+	@NotBlank
 	private String userId;
 	private String password;
 	private String userName;
