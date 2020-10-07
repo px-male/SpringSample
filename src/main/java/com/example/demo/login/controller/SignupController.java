9144fcc9c8836c570a9eea4387e1827fa3601dd9
1131051055614 <1131051055614@vpckr168.nsl.ad.nec.co.jp>
2020-10-07 19:55:52 +0900

    バリデーションチェックまで実装

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
@@ -35,7 +37,7 @@ public class SignupController {
 	}
 
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
 
 		if(bindingResult.hasErrors()) {
 		//GET リクエスト用のメソッドを呼び出し、登録画面に戻る
