93c689c9cade534d1042e4dfd7a48a534fb3d982
px-male <px-male@ppp>
2020-10-20 08:02:30 +0900

    画面作成

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -17,6 +17,8 @@ public class LogAspect {
 		System.out.println("メソッド開始:"+jp.getSignature());
 		
 		try {
+			
+			//メソッド実行
 			Object result = jp.proceed();
 			
 			System.out.println("メソッド終了:"+jp.getSignature());
@@ -26,6 +28,27 @@ public class LogAspect {
 		} catch(Exception e) {
 			
 			System.out.println("メソッド異常終了:"+jp.getSignature());
+			
+			e.printStackTrace();
+			
+			throw e;
+		}
+		
+	}
+	
+	// UserDaoクラスのログ出力
+	@Around("execution(* *..*.*UserDao*.*(..))")
+	public Object daoLog(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("メソッド開始" + jp.getSignature());
+		
+		try {
+			Object result = jp.proceed();
+			
+			System.out.println("メソッド終了" + jp.getSignature());
+			
+			return result;
+		} catch (Exception e) {
+			System.out.println("メソッド異常終了" + jp.getSignature());
 			
 			e.printStackTrace();
 			
--- src/main/java/com/example/demo/login/domain/model/User.java
+++ src/main/java/com/example/demo/login/domain/model/User.java
@@ -1,5 +1,15 @@
 package com.example.demo.login.domain.model;
 
-public class User {
+import java.util.Date;
+import lombok.Data;
 
+@Data
+public class User {
+	private String userId; //ユーザID
+	private String password; 
+	private String userName;
+	private Date birthday;
+	private int age;
+	private boolean marriage;
+	private String role;
 }
